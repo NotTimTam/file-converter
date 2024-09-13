a8a95d10a562b3d6d50dd7477f4d25deb3f26f20
NotTimTam <timothyrp6@gmail.com>
2024-09-13 13:30:32 -0700

    Updated module error messages.

--- util/Module.js
+++ util/Module.js
@@ -146,7 +146,7 @@ export default class Module {
 						!customReturn)
 				)
 					throw new SyntaxError(
-						`(${label}) Invalid 'from' value provided to Module constructor config. Expected a valid MIME type, file extension, or array of those values.`
+						`Invalid mimetype provided to Module constructor config.from array. Expected a valid MIME type, or file extension, but got: "${label}".`
 					);
 			}
 		else if (
